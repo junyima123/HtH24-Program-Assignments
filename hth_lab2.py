2988cfd70ff6a16871c9c52bb3dbf4cd6530ef79
Junyi Ma <joonxin@gmail.com>
2024-06-19 06:58:19 -0700

    Created New variables and used print() and type()

--- hth_lab2.py
+++ hth_lab2.py
@@ -1,1 +1,34 @@
+# Hello World 
+
 print("Hello World")
+
+# String 
+name = "Lebron"
+
+# Integer  
+
+age = 35
+
+# Floating Point 
+
+salary = 37.44
+
+# Boolean
+
+plays_basketball = True
+
+# String
+ 
+jersey_number = "23"
+car_name = "Tesla"
+x = 50
+my_first_name = "Junyi"
+
+print(name, type(name))
+print(age, type(age))
+print(salary, type(salary))
+print(plays_basketball, type(plays_basketball))
+print(jersey_number, type(jersey_number))
+print(car_name, type(car_name))
+print(x, type(x))
+print(my_first_name, type(my_first_name))
